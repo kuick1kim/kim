9a8f655fc6a14ee911f112f4f279e2c149a566c7
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-05-25 06:38:13 +0900

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -73,8 +73,6 @@ print('데이터프레임 변환')
 
 
 
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-BASE_DIR = os.getcwd()
 
 
 with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
@@ -120,12 +118,12 @@ encoders.encode_base64(part)
 part.add_header('Content-Disposition', "attachment", filename='NAVER_{}.xlsx'.format(date)) # 첨부파일 이름
 msg.attach(part)
 
-# part = MIMEBase('application', "octet-stream")
-# with open('news.json', 'rb') as file:
-#     part.set_payload(file.read())
-# encoders.encode_base64(part)
-# part.add_header('Content-Disposition', "attachment", filename='news.json') # 첨부파일 이름
-# msg.attach(part)
+part = MIMEBase('application', "octet-stream")
+with open('news.json', 'rb') as file:
+    part.set_payload(file.read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment", filename='news.json') # 첨부파일 이름
+msg.attach(part)
 
 
 # 메일 보내고 서버 끄기
