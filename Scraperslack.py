7645286da54d04c47cde79595c89efc89f8928b6
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-05-26 06:14:41 +0900

    Add files via upload

---
+++ Scraperslack.py
@@ -0,0 +1,15 @@
+import requests
+import json
+def post_message(channel, text): 
+    SLACK_BOT_TOKEN = "xoxb-2112022842768-2109250491412-iLtvZQDrhkdlWRVXgcJfueBo"
+    headers = {
+        'Content-Type': 'application/json', 
+        'Authorization': 'Bearer ' + SLACK_BOT_TOKEN
+        }
+    payload = {'channel': channel,'text': text}
+    r = requests.post('https://slack.com/api/chat.postMessage', 
+        headers=headers, 
+        data=json.dumps(payload)
+        )
+if __name__ == '__main__':
+    post_message("#stock", "깃허브에서 자동으로 슬랙으로 메세지를 보내고 있습니다")
