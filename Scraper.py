8f9b2b642d048902683fa54c747a3eaa9cf672f5
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-06-30 11:12:24 +0900

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -1,75 +1,73 @@
-from bs4 import BeautifulSoup
-import json
-import requests
+import requests 
+from bs4 import BeautifulSoup 
 import re
-import time, os
-import pandas as pd
 from datetime import datetime
+import pandas as pd
+from pandas import DataFrame
+import os
+from datetime import datetime
+import time
+import csv
+
+import requests
+import json
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
-from pandas import DataFrame
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-print('뉴스기사 스크래핑 시작')
-
-req = requests.get('https://www.yna.co.kr/news?site=navi_latest_depth01')
-time.sleep(3)
-req.encoding= None
-html = req.content
-soup = BeautifulSoup(html, 'html.parser')
-
-
-datas = soup.select('#container > div > div > div.section01 > section > div.list-type038 > ul > li')
-print('There are %d reviews avaliable!' % len(datas))
-
-
-df = pd.DataFrame(columns=['기사제목', '기사내용', '기사링크'])
-
-
-data = {}
-
-for title in datas:   
-    bbb2 = title.find("div" , class_='item-box01')
-    bbb1 = title.find("div" , class_='news-con')
-    try :
-        name99 = bbb1.find_all('a')[0].text.replace('\n', '')
-        name = name99.replace('\"', '>" ')
-        url = 'http:'+title.find('a')['href']
-        name2 = title.find_all('p', class_='lead')[0].text
-        name3 = name2.replace('\n', '')
-    except :
-        pass
-    data[name] = '기사요약-> ' + name3 + '            URL 주소는 여기---> ' + url
-    df = df.append({
-    '기사제목': name,
-    '기사내용': name3,
-    '기사링크': url
-  }, ignore_index=True)
-
-                          
-        
-    # print('기사요약')
 
 
 
 
 
 
-with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
-    json.dump(data, json_file, ensure_ascii = False, indent='\t')
-    print('저장됨')
 
-print('뉴스기사 스크래핑 끝')
 
-news_df = DataFrame(df).T
-folder_path = os.getcwd()
-# print(df)
-# xlsx_file_name = 'NAVER_{}.xlsx'.format(date)
-# news_df.to_excel(xlsx_file_name)
+
+
+
+
+date = str(datetime.now())
+date = date[:date.rfind(':')].replace(' ', '_')
+date = date.replace(':','시') + '분'
+
+
+
+url0 = 'http://www.stocksignals.co.kr:8080/stockguest/0102.php'
+url = url0
+
+raw = requests.get(url)
+html = BeautifulSoup(raw.text, 'html.parser' )
+table = html.select(".boxstyle")[2]
+row = table.select("tr" )[2] #10번째 줄을 의미함 자본이 늘어나는지
+table2 = row.select(".boxstyle") #1번째 칸 추출을 의미함 자본이 늘어나는지
+# rowrow = table2.select("td")
+
+infoTable = row.find("table",{"class":"boxstyle"})
+
+
+df = pd.DataFrame()
+infoPrint =[]
+for a in infoTable.find_all("tr"):
+    infolist = []
+    for b in a.find_all("td"):
+        info = b.get_text()
+        infolist.append(info)
+    # print(infolist)
+    infoPrint.append(infolist)
+    # df = df.append(infolist)
+# print(infoPrint)
+df2 = df.append(infoPrint)
+
+save_df = pd.DataFrame(df2)
+print(df2)
+df2 = df2.replace('°³ÀÎ','개인').replace('Åõ½Å','투신').replace('Áõ±Ç','기관').replace('¿Ü±¹ÀÎ','외국인').replace('ÄÝ¿É¼Ç (´ÜÀ§:°è¾à¼ö)','콜옵션').replace('Ç²¿É¼Ç (´ÜÀ§:°è¾à¼ö)','풋옵션').replace('Çà»ç°¡','행사가').replace('Á¾°¡','현재가격')
+# folder_path = os.getcwd()
+df2.to_csv('오늘의옵션가격{d}.csv'.format(d = date), index=False, encoding='utf-8-sig')
+# os.startfile(folder_path)
+
+
 
 
 # 보내는 사람 정보
@@ -85,30 +83,32 @@ you = "kuick1@hanmail.net"
 
 # 메일 기본 정보 설정
 msg = MIMEMultipart('alternative')
-msg['Subject'] = "김민상이 보냈습니다"
+msg['Subject'] = "김민상이 오늘의 옵션가격 보냈습니다"
 msg['From'] = me
 msg['To'] = you
 
 # 메일 내용 쓰기
-content = str(df)
+link1 = "http://www.stocksignals.co.kr:8080/stockguest/0102.php"
+
+link2 = "https://finance.daum.net/domestic/investors/DERIVATIVES"
+content = str(link2) + str(link1) + str(df2)
 part2 = MIMEText(content, 'plain')
 msg.attach(part2)
-# print(data)
+
 
 
 
 part = MIMEBase('application', "octet-stream")
-with open('news.json', 'rb') as file:
+with open('오늘의옵션가격{d}.csv'.format(d = date), 'rb') as file:
     part.set_payload(file.read())
 encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment", filename='news.json') # 첨부파일 이름
+part.add_header('Content-Disposition', "attachment", filename='오늘의옵션가격{d}.csv'.format(d = date)) # 첨부파일 이름
 msg.attach(part)
+
 
 
 # 메일 보내고 서버 끄기
 s.sendmail(me, you, msg.as_string())
 s.quit()
 print()
-print('메일까지 잘......보냈습니다.')
-
-
+print('이메일까지 잘......보냈습니다.')
