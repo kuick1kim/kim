977671d3b2056bb5554834bd6202a5f0dbd4b572
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-07-02 14:03:48 +0900

    Update Scraper_opt.py

--- Scraper_opt.py
+++ Scraper_opt.py
@@ -4,12 +4,13 @@ import datetime
 import smtplib
 from email.mime.text import MIMEText
 import os
+
 from os.path import join, dirname
 from dotenv import load_dotenv
 
 
 # 고정 변수
-dirver_loc = os.path.realpath(__file__)[:-11]+'/chromedriver'
+dirver_loc = os.path.realpath(__file__)[:-10]+'/chromedriver'
 
 joonggonara_url = 'https://cafe.naver.com/joonggonara.cafe?iframe_url=/ArticleList.nhn%3Fsearch.clubid=10050146%26search.boardtype=L%26viewType=pc'
 
